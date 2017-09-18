426e3e05d507044c1058f0764f9d787437709467
Brian Downs <brian.downs@gmail.com>
2017-09-18 10:23:23 -0700

    issue-6

--- middleware/auth.go
+++ middleware/auth.go
@@ -11,7 +11,8 @@ import (
 
 const (
 	// AuthHeader holds the header field name
-	AuthHeader = "X-Raven-Token"
+	AuthHeader = "X-Hashknife-Token"
+
 	// Path
 	Path = "Request-Path"
 )
@@ -19,48 +20,49 @@ const (
 // HashknifeRequestAuthenticator
 type HashknifeRequestAuthenticator struct {
 	authToken    *string
+	userAgent    *string
 	skippedPaths []string
 }
 
 // NewHashknifeRequestAuthenticator
-func NewHashknifeRequestAuthenticator(authToken *string) *HashknifeRequestAuthenticator {
+func NewHashknifeRequestAuthenticator(authToken, userAgent *string) *HashknifeRequestAuthenticator {
 	return &HashknifeRequestAuthenticator{
 		authToken: authToken,
+		userAgent: userAgent,
 		skippedPaths: []string{
 			"/geo-api/healthcheck",
+			"/api/healthcheck",
 		},
 	}
 }
 
-// skipAuth
+// skipAuth is used to not check for an authentication token for a given path
 func (a *HashknifeRequestAuthenticator) skipAuth(ctx context.Context) bool {
-	if a.authToken == nil {
+	if a.authToken == nil || a.userAgent == nil {
 		return true
 	}
-
 	path, ok := ctx.Value(Path).(string)
 	if !ok {
 		return false
 	}
-
 	for _, s := range a.skippedPaths {
 		if strings.HasPrefix(path, s) {
 			return true
 		}
 	}
-
 	return false
 }
 
-// verifyTokenHeader
+// verifyTokenHeader concludes that the given headers are or aren't valid
 func (a *HashknifeRequestAuthenticator) verifyTokenHeader(ctx context.Context) error {
 	if a.skipAuth(ctx) {
 		return nil
 	}
 	authToken, ok := ctx.Value(AuthHeader).(string)
+	userAgent, ok := ctx.Value("User-Agent").(string)
 	if !ok {
 		return endpoints.NewForbiddenError()
-	} else if authToken != *a.authToken {
+	} else if authToken != *a.authToken || userAgent != *a.userAgent {
 		return endpoints.NewForbiddenError()
 	}
 	return nil
@@ -82,6 +84,7 @@ func (a *HashknifeRequestAuthenticator) EndpointAuthenticate() kitendpoint.Middl
 // KitServerBefore
 func KitServerBefore(ctx context.Context, r *http.Request) context.Context {
 	ctx = context.WithValue(ctx, AuthHeader, r.Header.Get(AuthHeader))
+	ctx = context.WithValue(ctx, "User-Agent", r.Header.Get("User-Agent"))
 	ctx = context.WithValue(ctx, Path, r.URL.Path)
 	return ctx
 }
